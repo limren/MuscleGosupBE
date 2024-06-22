75e110d19180e82178857dd0db7f88a0fe480dc4
BOYER AXEL <axel.boyer2@etu.unistra.fr>
2024-06-22 16:23:28 +0200

    Project init - Spring Boot app

---
+++ src/main/java/muscleGosup/MuscleGosupBackendApplication.java
@@ -0,0 +1,13 @@
+package muscleGosup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleGosupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuscleGosupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/muscleGosup/spring_boot/MuscleGosupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package muscleGosup.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleGosupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
